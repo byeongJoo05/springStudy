c9e7c0f16f2fd5d5ada8546d6e55c67fb9901fc2
SBJ <thsqudwn05@gmail.com>
2022-09-08 12:06:37 +0900

    4장

--- src/test/java/org/zerock/springex/mapper/TimeMapperTests.java
+++ src/test/java/org/zerock/springex/mapper/TimeMapperTests.java
@@ -13,11 +13,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 public class TimeMapperTests {
 
     @Autowired(required = false)
-    private TimeMapper timeMapper;
+    private TimeMapper timeMapper2;
 
     @Test
     public void testGetTime() {
 
-        log.info(timeMapper.getTime());
+        log.info(timeMapper2.getTime());
     }
 }
